62666954333f3bd8aaaa550c7468fc935a0639b4
Alexandre Mennelet <amennelet@allflex-europe.com>
2017-10-20 16:32:05 +0200

    read all lines from a file

--- test/extractregex.test.js
+++ test/extractregex.test.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
 
 describe('extracting regex from file', () => {
     it('read all lines from a file', () => {
-        expect(false).toBeTruthy();
+        var totalLine = 0;
+        const lineReader = readline.createInterface({ input: fs.createReadStream(path.join(__dirname, 'Allflex_L1InkUnloadTrayProcessor_PLCBugOnReject.log')) });
+        lineReader.on('line', (line) => {
+            totalLine++;
+        });
+        lineReader.on('close', () => {
+            expect(470).toBe(totalLine);
+        });
     });
 });
