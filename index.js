be7f8e4a270ad5c4ed536654ff06acc6973132f2
Alexandre Mennelet <amennelet@allflex-europe.com>
2017-10-21 13:56:41 +0200

    Add console program

---
+++ index.js
@@ -0,0 +1,16 @@
+/* eslint no-console : off */
+import commander from 'commander';
+import ExtractRegex from './module/extractregex';
+
+commander
+    .version('1.0.0')
+    .option('-f, --file <file>', 'file to parse')
+    .option('-r, --regex [regex]', 'RegEx to extract')
+    .parse(process.argv);
+
+console.log(`Parsing ${commander.file} with ${commander.regex}`);
+
+const extractRegex = new ExtractRegex();
+extractRegex.on('found', value => console.log(value));
+extractRegex.on('close', () => console.log('\nDone.'));
+extractRegex.parseFile(commander.file, new RegExp(commander.regex));
